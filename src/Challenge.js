f89ab2ff6701273a1efc9f82ea89bd95daee3070
Theo Fortune <theofortunefunction@gmail.com>
2023-08-15 04:32:23 +0100

    a little progress in the challenge

--- src/Challenge.js
+++ src/Challenge.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Challenge = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const subtractCount = () => {
     setCount((c) => count - step);
   };
@@ -17,6 +17,45 @@ const Challenge = () => {
     setStep((s) => step + 1);
   };
 
+  // *Get day name
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = new Date().getDay();
+  const dayName = dayNames[day];
+  console.log(dayName);
+
+  // *Get month name
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = new Date().getMonth();
+  const monthName = monthNames[month];
+  console.log(monthName);
+
+  // *Get date
+  const date = new Date().getDate()
+
+  // *Get year
+  const year = new Date().getFullYear()
+
   return (
     <div
       style={{
@@ -28,13 +67,15 @@ const Challenge = () => {
     >
       <div className="step">
         <button onClick={subtractStep}>-</button>
-        Step: {step}<button onClick={addStep}>+</button>
+        Step: {step}
+        <button onClick={addStep}>+</button>
       </div>
       <div className="count">
         <button onClick={subtractCount}>-</button>
         Count: {count}
         <button onClick={addCount}>+</button>
       </div>
+      <p>Today is {dayName} {monthName} {date} {year}</p>
     </div>
   );
 };
